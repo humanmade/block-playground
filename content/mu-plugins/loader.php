2f6c0f486ff95e6504305beceb565025ec8f96e9
K. Adam White <kadamwhite@users.noreply.github.com>
2019-07-12 21:52:25 -0400

    Add humanmade/asset-loader mu-plugin
    Improve Loader (#1)
    Use is_readable instead of file_exists.

---
+++ content/mu-plugins/loader.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: HM MU Plugin Loader
+ * Description: Loads the MU plugins required to run the site
+ * Author:      Human Made Limited
+ * Author URI:  https://humanmade.com/
+ * Version:     1.0
+ *
+ * @package block-playground
+ */
+
+if ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) {
+	return;
+}
+
+// Plugins to be loaded for any site.
+$global_mu_plugins = [
+	'asset-loader/asset-loader.php',
+];
+
+// Load the plugin files, if they exist.
+foreach ( $global_mu_plugins as $file ) {
+	if ( is_readable( WPMU_PLUGIN_DIR . '/' . $file ) ) {
+		include_once WPMU_PLUGIN_DIR . '/' . $file;
+	}
+}
